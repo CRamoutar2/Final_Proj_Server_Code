4448713df4ec2fd38d4e39fdc7687d2c512a4846
GuyotDavid <48332768+GuyotDavid@users.noreply.github.com>
2021-12-18 22:26:40 -0500

    Update seedDB.js
    add emails, addresses and gpas to seed

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -2,17 +2,24 @@ const { Campus, Student } = require('../models');
 
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
-		name: "Hunter College",
+		name: "Hunter College",		
+		address: "695 Park Avenue, New York, NY 10065",
+		imageURL: "https://s29068.pcdn.co/wp-content/uploads/68th-street-campus-768x432.jpg",
 		description: "This is a school in NYC."
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
+		address: "Massachusetts Hall, Cambridge, MA 02138",
+		imageURL: "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F1209892117%2F0x0.jpg",
 		description: "This is a school in MA."
 	});
 
 	const dummy_student = await Student.create({
 			firstname: "Joe",
       lastname: "Shmo",
+		email: "JoeShmo@gmail.com",
+		imageURL: "https://th-thumbnailer.cdn-si-edu.com/5a79C6jJ8BrChMX5tgEKiMI_qqo=/1000x750/filters:no_upscale():focal(792x601:793x602)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/52/e4/52e44474-c2dc-41e0-bb77-42a904695196/this-image-shows-a-portrait-of-dragon-man-credit-chuang-zhao_web.jpg",
+		gpa: 1.2
 		});
 
 	await dummy_student.setCampus(dummy_campus);
